f5913c44a4ff498a965e62f3642e5f7923178741
Christina Cruder <53585660+ckatr13@users.noreply.github.com>
2021-04-25 21:33:10 -0400

    Add files via upload

--- project.mjs
+++ project.mjs
@@ -24,6 +24,7 @@ export function fillInfo(projObj) {
         const li = document.getElementById("services").appendChild(newLi);
         const s = document.createTextNode(projObj.services[i] + "/");
         const lastS = document.createTextNode(projObj.services[i]);
+        console.log(projObj.services.indexOf(i));
         if(projObj.services.indexOf(i) < projObj.services.length - 1) {
             li.appendChild(s);
         } else {
